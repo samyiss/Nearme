5a0fb34b5e267055250fe88f58528b60981ed8bc
eusebe13 <e1821806@cmaisonneuve.qc.ca>
2022-10-05 01:40:31 -0400

    manque de couleur pale

--- NearMe_app/app/src/main/java/com/example/nearme_app/MainActivity.kt
+++ NearMe_app/app/src/main/java/com/example/nearme_app/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.nearme_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navbar : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navbar = findViewById(R.id.bottom_navigation)
+        navbar.setOnItemSelectedListener{
+            when (it.itemId) {
+                R.id.Home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.Recherche -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.More -> {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.Mail -> {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.Profile -> {
+                    val intent = Intent(this, ProfileGuestActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
