6474962d4908b5edcf2697315612e71433b22984
samy <e2072931@cmaisonneuve.qc.ca>
2022-10-05 01:40:10 -0400

    update and delete start redirection

--- NearMe_app/app/src/main/java/com/example/nearme_app/profileloggedinActivity.kt
+++ NearMe_app/app/src/main/java/com/example/nearme_app/profileloggedinActivity.kt
@@ -1,11 +1,17 @@
 package com.example.nearme_app
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class profileloggedinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profileloggedin)
+
+        val resetPass = findViewById<ImageView>(R.id.resetPassword)
+        val updateProfile = findViewById<ImageView>(R.id.updateProfile)
+
     }
 }
