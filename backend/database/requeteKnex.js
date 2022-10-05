430dc411455194a74ebb404704539d8d3f02981b
samy <e2072931@cmaisonneuve.qc.ca>
2022-10-04 20:50:54 -0400

    service post get

--- backend/database/requeteKnex.js
+++ backend/database/requeteKnex.js
@@ -4,10 +4,25 @@ const chaineConnexion = require('./dbConnexion');
 const knex = knexModule(chaineConnexion);
 
 // Requete de test
-function getIppesAll() {
-    return knex('categories');
+async function addService(service) {
+    let row = []
+    await knex('services').insert(service).then(async () => {
+        await knex('services').select().then((rows) => {
+            row.push(rows[rows.length - 1]);
+        })
+    }).catch((err) => {
+        console.log(err);
+        throw err;
+    })
+    return row;
+}
+
+//get les données de la table services
+function getServices() {
+    return knex('services');
 }
 
 module.exports = {
-    getIppesAll
+    addService,
+    getServices
 };
--- backend/database/service.js
+++ backend/database/service.js
@@ -2,10 +2,64 @@ const { getAuth, signInWithEmailAndPassword, sendEmailVerification, createUserWi
 const { getDatabase, ref, set, get, child, update } = require("firebase/database");
 
 const { fapp } = require('./firebaseconf');
-const { getIppesAll } = require("./requeteKnex");
+const { addService, getServices } = require("./requeteKnex");
 
-// add new service to sql server
 exports.createService = async(req,res) =>{
-    user = await getIppesAll();
-    res.status(200).json(user)
+    const auth = getAuth(fapp);
+    const user = auth.currentUser;
+
+    if (user === null) {
+        // choix des infos a envoyer selon la banque de données choisi
+        const { Id_categorie, nomService, description, prix, photoCouverture } = req.body;
+        try {
+            if (Id_categorie === undefined && nomService === undefined)
+                return res.status(400).json({ success : false, message: 'paramètre manquant'});
+
+            const DataToSend = {
+                Id_user: "user.uid",  
+                Id_categorie: Id_categorie,
+                nomService: nomService,
+                description: description? description : '',
+                prix: prix? prix : 0,
+                photoCouverture: photoCouverture? photoCouverture : '',
+                datePublication: new Date().toLocaleString('fr-FR', 'Canada/Montréal'),
+            };
+            // ajout de données
+            const row = await addService(DataToSend)
+            if(row !== []) {
+                return res.status(201).json({ success : true, message: 'le service a été ajouté' });
+            } else {
+                return res.status(500).json({ success: false, message: "une erreur est survenue lors de l'ajout" });
+            }
+        } catch (error) {
+            return res.status(500).json({ success: false, message: error });
+        }
+    } else {
+        return res.status(401).json({ success: false, message: 'Vous n\'êtes pas connecté' });
+    }
+};
+
+
+exports.getAllServices = async(req,res) =>{
+    try {
+        const services = await getServices();
+        if(services !== []) {
+            let data = [];
+            services.forEach((service) => {
+                dataDisplay = {
+                    Id_service: service.id_service,
+                    nomService: service.nomService,
+                    prix: service.prix,
+                    photoCouverture: service.photoCouverture,
+                    datePublication: service.datePublication,
+                }
+                data.push(dataDisplay);
+            });
+            return res.status(200).json( data );
+        } else {
+            return res.status(404).json({ success: false, message: "aucun service trouvé" });
+        }
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "une erreur est survenue lors de la récupération des services" });
+    }
 }
