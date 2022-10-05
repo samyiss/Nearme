ac076501b12e86b6c1edcde85cb9ada2b98920bd
imransahy_ <e1937658@cmaisonneuve.qc.ca>
2022-10-04 21:07:24 -0400

    Auto stash before merge of "FrontEnd" and "origin/FrontEnd"

--- NearMe_app/app/src/main/java/com/example/nearme_app/LoadingActivity.kt
+++ NearMe_app/app/src/main/java/com/example/nearme_app/LoadingActivity.kt
@@ -1,10 +1,14 @@
 package com.example.nearme_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,7 +18,22 @@ class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_loading)
-        val rv = this.findViewById<RecyclerView>(id.logo_loading)
-        rv.layoutManager = LinearLayoutManager(applicationContext)
+        val logoLoading = findViewById<ImageView>(R.id.logoLoading)
+        val textLoading = findViewById<TextView>(R.id.textLoading)
+
+        val topAnimation = AnimationUtils.loadAnimation(this, R.anim.top)
+        val botAnimation = AnimationUtils.loadAnimation(this, R.anim.bot)
+
+        logoLoading.startAnimation(topAnimation)
+        textLoading.startAnimation(botAnimation)
+
+        val splashScreenTimeOut = 4000
+        val homeIntent = Intent(this@LoadingActivity, MainActivity::class.java)
+
+        Handler().postDelayed({
+            startActivity(homeIntent)
+            finish()
+        }, splashScreenTimeOut.toLong())
+
     }
 }
